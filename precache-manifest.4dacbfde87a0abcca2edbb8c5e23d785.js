b8e676962fb3ea815f362c3bfdbea8dc2656d8de
uttam-dholariya <uvdholariya@gmail.com>
2023-05-08 11:08:26 +0530

    Updates

---
+++ precache-manifest.4dacbfde87a0abcca2edbb8c5e23d785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7d6ab63aa0dae0c1895",
+    "url": "/textUttam/static/css/main.75880463.chunk.css"
+  },
+  {
+    "revision": "b7d6ab63aa0dae0c1895",
+    "url": "/textUttam/static/js/main.b7d6ab63.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textUttam/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textUttam/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "c01aebfe29f04d71d370",
+    "url": "/textUttam/static/js/runtime~main.c01aebfe.js"
+  },
+  {
+    "revision": "208706793c943a3640b6f6f833adb5c3",
+    "url": "/textUttam/index.html"
+  }
+];
